4a8cdf5c07ed87cf734e5290b7c336c93f76c2f3
sametavcii <sametavc05@gmail.com>
2022-06-18 17:26:04 +0300

    Lecturer info added

--- discord/handle_message.go
+++ discord/handle_message.go
@@ -13,8 +13,9 @@ import (
 
 func Announce(url string, s *discordgo.Session, m *discordgo.MessageCreate) {
 
-	go routers.Router()
+	go routers.RouterAnnounce()
 	time.Sleep(5 * time.Second)
+
 	resp, err := http.Get(url)
 	if err != nil {
 		log.Fatal(err)
@@ -36,6 +37,40 @@ func Announce(url string, s *discordgo.Session, m *discordgo.MessageCreate) {
 		_, err = s.ChannelMessageSend(m.ChannelID, announ[i].Title)
 		_, err = s.ChannelMessageSend(m.ChannelID, announ[i].Description)
 		_, err = s.ChannelMessageSend(m.ChannelID, announ[i].Url)
+
+		if err != nil {
+			fmt.Println("Don't Send File ", err)
+		}
+	}
+}
+
+func Lectures(url string, s *discordgo.Session, m *discordgo.MessageCreate) {
+
+	go routers.RouterLectures()
+	time.Sleep(5 * time.Second)
+
+	resp, err := http.Get(url)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var lectures []Lecturer
+	err = json.Unmarshal(body, &lectures)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(lectures)
+	for i := range lectures {
+		_, err = s.ChannelMessageSend(m.ChannelID, lectures[i].Name)
+		_, err = s.ChannelMessageSend(m.ChannelID, lectures[i].Email)
+		_, err = s.ChannelMessageSend(m.ChannelID, lectures[i].Phone)
+		_, err = s.ChannelMessageSend(m.ChannelID, lectures[i].Workspace)
 
 		if err != nil {
 			fmt.Println("Don't Send File ", err)
